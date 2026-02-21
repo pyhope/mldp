0d201a4deff8ac113d096646caab560ded3ec7b4
pyhope <ypeng0082@gmail.com>
2026-02-21 17:08:50 -0500

    update

--- recal_dpdata.py
+++ recal_dpdata.py
@@ -12,22 +12,33 @@ import os
 from shutil import copy
 #from shared_functions import load_paths
 import dpdata
+import numpy as np
 #import glob
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--deepmd","-d",help="deepmd path file")
 parser.add_argument("--inputfile","-if",help="input files for vasp cal, default is cwd+inputs, if input, please input the absolute path")
-parser.add_argument("--temperature","-t",default=4000,type=int,help="simulation temperature")
+parser.add_argument("--temperature","-t",default=None,type=int,help="simulation temperature")
 parser.add_argument("--step","-s",default=1,type=int,help="step")
 parser.add_argument("--range","-r",type=str,help="0-2, means from 0 to 2, default is for all folders")
 parser.add_argument("--recal_dir_name","-rd",default='recal',help="Path to the recal directory")
 parser.add_argument("--spin","-sp",action='store_true',help="add magmom and nupdown (IS Fe) in INCAR, default is False")
+parser.add_argument("--potcar_by_elements","-pe",action='store_true',help="generate POTCAR by elements, default is False")
+parser.add_argument("--calc_nband_from_nelec","-cn",action='store_true',help="calculate Nband from Nelec in POSCAR, default is False")
+
 # parser.add_argument("--run_vasp","-rv",help="run vasp?, default without input is Yes")
 # parser.add_argument("--sub_command","-sc", default='/u/systems/UGE8.6.4/bin/lx-amd64/qsub',help="job submission command: default is /u/systems/UGE8.6.4/bin/lx-amd64/qsub")
 
 args   = parser.parse_args()
 temp = args.temperature
+elem_dict = {'Fe':14,'Mg':8,'O':6,'W':12, 'Pt':10}
+
+if temp is None:
+    print('***temperature is not provided, read from set.000/fparam.npy')
+    fparam = np.load(os.path.join(args.deepmd,'set.000','fparam.npy'))
+    temp = float(fparam.flatten()[0]) / 8.6173303e-5
+    print('***temperature read from fparam.npy is',temp)
 
 def lmp2pos(ls,sel_nsw,copybs=False):
     """
@@ -63,7 +74,7 @@ def lmp2pos(ls,sel_nsw,copybs=False):
             target_path    = os.path.join(recal_path,str(i+1))                   
             ls.to_vasp_poscar(os.path.join(target_path,'POSCAR'),frame_idx=i)
             with open(os.path.join(inputfile,'INCAR')) as f:
-                content = f.read().replace('temp_value', str(temp))
+                content = f.read().replace('temp_value', f'{temp:.1f}')
                 sigma_value = 8.6173303e-5 * temp
                 content = content.replace('sigma_value', f'{sigma_value:.4f}')
             tmp_incar_path = os.path.join(inputfile,'INCAR_tmp')
@@ -71,19 +82,42 @@ def lmp2pos(ls,sel_nsw,copybs=False):
                 f.write(content)
             copy(tmp_incar_path,os.path.join(target_path,'INCAR'))
             copy(os.path.join(inputfile, 'KPOINTS'), target_path)
-            os.symlink(os.path.join(inputfile, 'POTCAR'), os.path.join(target_path, 'POTCAR'))
+            
+            incar_path = os.path.join(target_path, "INCAR")
+            poscar_path = os.path.join(target_path, "POSCAR")
+            with open(poscar_path, "r") as f:
+                lines = f.readlines()
+            
+            elements = lines[5].split()
+            atoms_per_species = lines[6].split()
+            potcar_path = os.path.join(target_path, "POTCAR")
+            if args.potcar_by_elements:
+                with open(potcar_path, "w") as potcar_file:
+                    for element in elements:
+                        element_potcar_path = os.path.join(inputfile, "POTCAR_" + element)
+                        with open(element_potcar_path, "r") as element_potcar_file:
+                            potcar_file.write(element_potcar_file.read())
+            else:
+                os.symlink(os.path.join(inputfile, 'POTCAR'), potcar_path)
+
+            if args.calc_nband_from_nelec:
+                nelec = 0
+                for element, count in zip(elements, atoms_per_species):
+                    nelec += elem_dict[element] * int(count)
+                nband = int(nelec * 3 / 4)
+                
+                with open(incar_path, "r") as f:
+                    incar_content = f.read()
+                incar_content = incar_content.replace("nband_value", str(nband))
+                with open(incar_path, "w") as f:
+                    f.write(incar_content)
 
             if args.spin:
-                poscar_path = os.path.join(target_path, "POSCAR")
-                with open(poscar_path, "r") as f:
-                    lines = f.readlines()
-                atoms_per_species = lines[6].split()
                 Fe, Mg, Si, O = map(int, atoms_per_species)
 
                 magmom = f"{Fe}*2 {Mg}*0 {Si}*0 {O}*0"
                 nupdw = Fe * 2 + Mg * 0 + Si * 0 + O * 0
 
-                incar_path = os.path.join(target_path, "INCAR")
                 with open(incar_path, "r") as f:
                     incar_content = f.read()
 
